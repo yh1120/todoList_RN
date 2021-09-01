e03ec7d90607ef591ec1c03622579807fba6a46b
yh1120 <yonghun1120@gmail.com>
2021-09-01 22:28:47 +0900

    Style: Icons Directory moved

--- src/Constants/Images.ts
+++ src/Constants/Images.ts
@@ -1,6 +1,6 @@
 export const images = {
-  uncompleted: require('../Assets/Icons/check_box_outline.png'),
-  completed: require('../Assets/Icons/check_box.png'),
-  delete: require('../Assets/Icons/delete_forever.png'),
-  update: require('../Assets/Icons/edit.png'),
+  uncompleted: require('../../assets/Icons/check_box_outline.png'),
+  completed: require('../../assets/Icons/check_box.png'),
+  delete: require('../../assets/Icons/delete_forever.png'),
+  update: require('../../assets/Icons/edit.png'),
 };
